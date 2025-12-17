028eeb47b1b55e4374e07931a541ddbb495c57d3
hzaheertks <haniyah.zaheer@thekaustschool.org>
2025-12-17 20:14:01 +0300

    1

--- src/Project3.java
+++ src/Project3.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.time.LocalTime;
+
 public class Project3 {
+
+
+    ///  ADD METHODS HEREIUQWJKAFDNSIAWBJRKSGFND,IPERABSFJKDN
     public static void main(String[] args) {
-        //hi there
+        JFrame frame =  new JFrame("Murder Mysery!");
+        frame.setSize(700,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        output = new JTextArea();
+        
     }
 }
